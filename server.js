022f1ce58689827822d99facc3b7ec5e06a795f8
Gautamsingh45 <gautamsingh893591@gmail.com>
2024-01-12 10:24:35 +0530

    first

--- server.js
+++ server.js
@@ -28,11 +28,11 @@ const Role = db.role;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
-db.mongoose
-  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+// mongoose.connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   })
+db.mongoose.connect("mongodb+srv://gautamsingh893591:V2jTulad1dfL6bE9@cluster0.vwrm2se.mongodb.net/student?retryWrites=true&w=majority")
   .then(() => {
     console.log("Successfully connect to MongoDB.");
     initial();
